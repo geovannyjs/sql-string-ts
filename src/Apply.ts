fc582210d0a870f6e5c9460cb86331bdc1ec4aad
Geovanny Silva <geovanny@vanit.co>
2024-01-21 22:09:52 -0300

    Apply types

---
+++ src/Apply.ts
@@ -0,0 +1,23 @@
+type ApplyAlias = {
+  alias?: boolean
+}
+
+type ApplyAs = {
+  as?: boolean
+}
+
+type ApplyPrefix = {
+  prefix?: boolean
+}
+
+type ApplyQuote = {
+  quote?: boolean
+}
+
+
+export type {
+  ApplyAlias,
+  ApplyAs,
+  ApplyPrefix,
+  ApplyQuote
+}
