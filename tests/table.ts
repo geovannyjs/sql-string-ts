c38a4023df0c8451bd9b1a8236896f395521671b
Geovanny Silva <geovanny@vanit.co>
2024-02-08 17:15:18 -0300

    tests table better descriptions

--- tests/table.ts
+++ tests/table.ts
@@ -7,25 +7,25 @@ import { user, movie, director } from './schema'
 
 describe('table', () => {
 
-  it('without config params', () => {
+  it('should return the table name when called without config', () => {
     assert.equal(table(user), 'user')
     assert.equal(table(movie), 'movie')
     assert.equal(table(director), 'director')
   })
 
-  it('with alias enabled', () => {
+  it('should return the table aliased when called with alias config enabled', () => {
     assert.equal(table(user, { alias: true }), 'user u')
     assert.equal(table(movie, { alias: true }), 'movie m')
     assert.equal(table(director, { alias: true }), 'director d')
   })
 
-  it('with quote enabled', () => {
+  it('should return the table quoted when called with quote config enabled', () => {
     assert.equal(table(user, { quote: true }), '`user`')
     assert.equal(table(movie, { quote: true }), '`movie`')
     assert.equal(table(director, { quote: true }), '`director`')
   })
 
-  it('with alias and quote enabled', () => {
+  it('should return the table aliased and quoted when called with alias and quote config enabled', () => {
     assert.equal(table(user, { alias: true, quote: true }), '`user` `u`')
     assert.equal(table(movie, { alias: true, quote: true }), '`movie` `m`')
     assert.equal(table(director, { alias: true, quote: true }), '`director` `d`')
