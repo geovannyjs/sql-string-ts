3f638e7f411a9d3a16561abd94f9eb58236eb96b
Geovanny Silva <geovanny@vanit.co>
2024-02-09 13:33:04 -0300

    tests column with better descriptions

--- tests/column.ts
+++ tests/column.ts
@@ -7,31 +7,31 @@ import { user, movie, director } from './schema'
 
 describe('column', () => {
 
-  it('without config params', () => {
+  it('should return the column name when called without config', () => {
     assert.equal(column(user.name), 'name')
     assert.equal(column(movie.name), 'name')
     assert.equal(column(director.name), 'name')
   })
 
-  it('with as enabled', () => {
+  it('should return the column name with as when called with as enabled', () => {
     assert.equal(column(user.name, { as: true }), 'name as u_name')
     assert.equal(column(movie.name, { as: true }), 'name as m_name')
     assert.equal(column(director.name, { as: true }), 'name as d_name')
   })
 
-  it('with prefix enabled', () => {
+  it('should return the column name prefixed when called with prefix enabled', () => {
     assert.equal(column(user.name, { prefix: true }), 'u.name')
     assert.equal(column(movie.name, { prefix: true }), 'm.name')
     assert.equal(column(director.name, { prefix: true }), 'd.name')
   })
 
-  it('with quote enabled', () => {
+  it('should return the column name quoted when called with quote enabled', () => {
     assert.equal(column(user.name, { quote: true }), '`name`')
     assert.equal(column(movie.name, { quote: true }), '`name`')
     assert.equal(column(director.name, { quote: true }), '`name`')
   })
 
-  it('with as, prefix and quote enabled', () => {
+  it('should return the column name with as, prefixed and quoted when called with as, prefix and quote enabled', () => {
     assert.equal(column(user.name, { as: true, prefix: true, quote: true }), '`u`.`name` as `u_name`')
     assert.equal(column(movie.name, { as: true, prefix: true, quote: true }), '`m`.`name` as `m_name`')
     assert.equal(column(director.name, { as: true, prefix: true, quote: true }), '`d`.`name` as `d_name`')
